acf2fe5cbfe5cc0a019aa88bdefbb9541a40267c
a-poor <45295232+a-poor@users.noreply.github.com>
2022-02-13 10:55:13 -0800

    Starting frontend dev
    * Clearing out frontend starter template
    * Adding editor.js and some basic plugins
    * Testing out some (unstyled & unconfigured) plugins

---
+++ frontend/src/MdEditor.svelte
@@ -0,0 +1,33 @@
+<script>
+	import EditorJS from '@editorjs/editorjs';
+  import Header from '@editorjs/header'; 
+  import List from '@editorjs/list'; 
+  import CheckList from '@editorjs/checklist';
+  // import LinkTool from '@editorjs/link';
+  import Table from '@editorjs/table';
+
+
+	let editor = new EditorJS({
+    holderId: 'md-editor',
+    autofocus: true,
+    tools: {
+      header: {
+        class: Header,
+        defaultLevel: 1,
+        levels: [1, 2, 3, 4, 5, 6],
+      },
+      list: List,
+      checklist: CheckList,
+      // linkTool: LinkTool,
+      table: {
+        class: Table,
+        inlineToolbar: true,
+      },
+    },
+  });
+</script>
+
+<div id="md-editor" />
+
+<style>
+</style>
